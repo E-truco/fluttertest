b36693ae7159ea234031263be6e39903d6b10f9c
CodyKoInABox <codykoinabox@gmail.com>
2024-07-01 14:31:57 -0300

    home screen

---
+++ trucoapp/lib/screens/home_screen.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'game_screen.dart';
+
+class HomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Truco Paulista'),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          child: Text('Start Game'),
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => GameScreen()),
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
